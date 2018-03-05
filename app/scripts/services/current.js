e088f3eacfe2991948430dfb486ebb128a5b8328
JensNordal <jensnordal@gmail.com>
2018-03-05 01:40:04 -0800

    Added API origin to API comment

--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -12,7 +12,7 @@ angular.module('chargingStationsApp')
     // Service logic
     // ...
 
-    // Public API
+    // Public API from NREL
     return $resource('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=:location&fuel_type=ELEC&radius=5.0&status=E&cards_accepted=all&limit=200&api_key=aPpj2Zmtm21V9j4douZuoEtDCNKpdxddl811Rk8a', {}, {
       query: {
         method:'GET',
